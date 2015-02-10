072d019746ae04d33879900744f6562f34e08bf2
Jesse Bannon <jbannon@localhost.yoloX420XobamaXkush>
2015-02-09 23:34:45 -0800

    Scope

--- src/main/java/breakable_windows/StartUp.java
+++ src/main/java/breakable_windows/StartUp.java
@@ -5,12 +5,14 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class StartUp extends JavaPlugin {
 	
-	private WindowListener window;
+	private BlockListener window;
+	private ScopeZoomListener scope;
 	
 	@Override
 	public void onEnable() {
-		this.window = new WindowListener();
+		this.window = new BlockListener();
 		this.getServer().getPluginManager().registerEvents(window, this);
+		this.getServer().getPluginManager().registerEvents(scope, this);
 		this.getLogger().info("Breakable Windows enabled!");
 
 	}
@@ -18,7 +20,8 @@ public class StartUp extends JavaPlugin {
 	@Override
 	public void onDisable() {
 		this.getLogger().info("Restoring glass...");
-		this.window.restoreGlass();
+		this.window.disable();
+		this.scope.disable();
 		this.getLogger().info("Restored glass! Breakable Windows disabled.");
 	}	
 }
